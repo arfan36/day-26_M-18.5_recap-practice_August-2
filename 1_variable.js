2f75178f37b824e1a2a2f6e79aae7d35957bbdbf
arfan36 <arfanur36@gmail.com>
2022-08-04 11:24:59 +0600

    first commit

---
+++ 1_variable.js
@@ -0,0 +1,15 @@
+/* 
+Variable
+1. What is JavaScript?
+2. How does JS Works? (If needed search google)
+3. What is Variable?
+4. Declare a variable (hints: 5 type)
+5. Types of a Variable? How can you find out type of variable? (If needed search google)
+6. Primitive and non-primitive data types
+7. Naming convention of JS variables
+8. Math Operation +, -, *, /, %
+9. Short hand: +=, -=, *=, /=
+10. ++, --
+11. parseInt, ParseFloat
+12. toFixed (If needed search google)
+ */
